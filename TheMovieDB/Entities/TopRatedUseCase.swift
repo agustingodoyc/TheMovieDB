8854c770c0c21f3cecfaafba0f853e1ce3c574a5
Agustín Godoy <a.godoy@globant.com>
2022-10-27 11:14:33 -0300

    Usecase blueprint

---
+++ TheMovieDB/Entities/TopRatedUseCase.swift
@@ -0,0 +1,29 @@
+//
+//  TopRatedUseCase.swift
+//  TheMovieDB
+//
+//  Created by Agustin Godoy Cosser on 26/10/2022.
+//
+
+import Foundation
+
+class TopRatedUseCase {/*
+    
+    // MARK: - Properties
+    
+    let dataManager: DataManagerProtocol
+    var delegate: TopRatedUCDelegate?
+    let viewModel: TopRatedViewModel = TopRatedViewModel()
+    
+    init(_ dataManager: DataManagerProtocol = DataManager()) {
+        self.dataManager = dataManager
+    }
+    
+    func execute(completionHandler: @escaping ([Movie]) -> Void){
+        dataManager.getData(Endpoints.topRated) { result in
+            DispatchQueue.main.async() {
+                completionHandler(result)
+            }
+        }
+    }*/
+}
---
+++ TheMovieDB/Interfaces/TopRatedUCDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  TopRatedUCDelegate.swift
+//  TheMovieDB
+//
+//  Created by Agustin Godoy Cosser on 26/10/2022.
+//
+
+import Foundation
+
+protocol TopRatedUCDelegate {
+    func getTopRated()
+}
