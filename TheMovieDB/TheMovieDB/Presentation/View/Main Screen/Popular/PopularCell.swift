1a7dde9e641bcd32f729fa85cd75c45734eb19ca
Angela <angela.lee@globant.com>
2022-12-01 13:58:08 -0300

    Added vote avarage

--- TheMovieDB/TheMovieDB/Presentation/View/Main Screen/Popular/PopularCell.swift
+++ TheMovieDB/TheMovieDB/Presentation/View/Main Screen/Popular/PopularCell.swift
@@ -15,6 +15,10 @@ class PopularCell: UITableViewCell {
         let poster = UIImageView()
         poster.layer.cornerRadius = 10
         poster.clipsToBounds = true
+        poster.image = UIImage(named: "TMDBImage")
+        poster.alpha = 0.8
+        poster.isOpaque = true
+        poster.backgroundColor = UIColor(red: 0.02, green: 0.13, blue: 0.27, alpha: 1)
         return poster
     }()
     lazy var title: UILabel = {
@@ -23,6 +27,8 @@ class PopularCell: UITableViewCell {
         title.clipsToBounds = true
         title.numberOfLines = 0
         title.adjustsFontSizeToFitWidth = true
+        title.font = .preferredFont(forTextStyle: .title1)
+        title.backgroundColor = .white.withAlphaComponent(0.7)
         return title
     }()
     
@@ -32,41 +38,52 @@ class PopularCell: UITableViewCell {
         releaseDate.clipsToBounds = true
         releaseDate.numberOfLines = 1
         releaseDate.adjustsFontSizeToFitWidth = true
+        releaseDate.textColor = .black.withAlphaComponent(0.5)
+        releaseDate.font = .preferredFont(forTextStyle: .title2)
+        releaseDate.backgroundColor = .white.withAlphaComponent(0.7)
         return releaseDate
+    }()
+    
+    lazy var voteAverage: UILabel = {
+        let voteAverage = UILabel()
+        voteAverage.layer.cornerRadius = 7
+        voteAverage.clipsToBounds = true
+        voteAverage.numberOfLines = 1
+        voteAverage.adjustsFontSizeToFitWidth = true
+        voteAverage.textColor = .white
+        voteAverage.font = .preferredFont(forTextStyle: .title3)
+        voteAverage.backgroundColor = UIColor(red: 0.02, green: 0.13, blue: 0.27, alpha: 1)
+        return voteAverage
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        backgroundColor = UIColor(red: 6, green: 34, blue: 68, alpha: 1)
-        setUpConstrains()
+        setUp()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setUpConstrains() {
+    func setUp() {
         addSubview(poster)
         setPosterConstrains()
         addSubview(title)
         setTitleConstraints()
         addSubview(releaseDate)
         setReleaseDateConstraints()
+        addSubview(voteAverage)
+        setVoteAverageConstraints()
         
     }
     
     func setPosterConstrains() {
         poster.translatesAutoresizingMaskIntoConstraints = false
-        poster.backgroundColor = UIColor(red: 6, green: 34, blue: 68, alpha: 1)
-        poster.image = UIImage(named: "TMDBImage")
         poster.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        poster.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8).isActive = true
-        poster.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8).isActive = true
-        poster.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
-        poster.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8).isActive = true
-        poster.alpha = 0.7
-        poster.isOpaque = true
-        
+        poster.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
+        poster.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+        poster.topAnchor.constraint(equalTo: topAnchor, constant: 5).isActive = true
+        poster.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5).isActive = true
     }
     
     func setTitleConstraints() {
@@ -74,17 +91,18 @@ class PopularCell: UITableViewCell {
         title.topAnchor.constraint(equalTo: poster.topAnchor, constant: 18).isActive = true
         title.leadingAnchor.constraint(equalTo: poster.leadingAnchor, constant: 18).isActive = true
         title.trailingAnchor.constraint(lessThanOrEqualTo: poster.trailingAnchor, constant: -18).isActive = true
-        title.font = .preferredFont(forTextStyle: .title1)
-        title.backgroundColor = .white.withAlphaComponent(0.5)
     }
     func setReleaseDateConstraints() {
         releaseDate.translatesAutoresizingMaskIntoConstraints = false
         releaseDate.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 8).isActive = true
         releaseDate.leadingAnchor.constraint(equalTo: poster.leadingAnchor, constant: 18).isActive = true
         releaseDate.trailingAnchor.constraint(lessThanOrEqualTo: poster.trailingAnchor, constant: -18).isActive = true
-        releaseDate.textColor = .black.withAlphaComponent(0.5)
-        releaseDate.font = .preferredFont(forTextStyle: .title2)
-        releaseDate.backgroundColor = .white.withAlphaComponent(0.7)
+    }
+    
+    func setVoteAverageConstraints() {
+        voteAverage.translatesAutoresizingMaskIntoConstraints = false
+        voteAverage.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -25).isActive = true
+        voteAverage.trailingAnchor.constraint(equalTo: poster.trailingAnchor, constant: -25).isActive = true
     }
     
     func loadPopularMovie(movie: PopularCellModel) {
@@ -95,5 +113,6 @@ class PopularCell: UITableViewCell {
             }
         }
         releaseDate.text = movie.releaseDate
+        voteAverage.text = String(movie.voteAverage ?? 0)
     }
 }
